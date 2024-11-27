d57e88514fb91a03e4f1e5ccb95e212f46c3d782
Kamlesh7049 <Kamlesh7049>
2024-11-27 22:12:30 +0530

    added persist

--- src/store.jsx
+++ src/store.jsx
@@ -1,10 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import { persistStore,persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web and AsyncStorage for react
+
 import myReducer from "./cartSlice";
+
+const persistConfig={
+    key:"root",
+    storage
+}
+const persistedReducer=persistReducer(persistConfig,myReducer);
 
 const store= configureStore({
     reducer:{
-     mycart:myReducer
+    //  mycart:myReducer
+     mycart:persistedReducer
     }
 })
 export default store;
+export const persistor=persistStore(store)
